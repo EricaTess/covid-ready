1323f3bb9d4d794c6bad37ab069e256b35256a78
EricaTess <sanchez.ericat@gmail.com>
2020-06-03 22:55:28 +0000

    create create measure and measure rating functions

--- crud.py
+++ crud.py
@@ -12,6 +12,26 @@ def create_user(username, email, password):
 
     return user
 
+def create_measure(measure, max_score):
+    """Create a new measure."""
+
+    measure = Measure(measure=measure, max_score)
+
+    db.session.add(measure)
+    db.session.commit()
+
+    return measure
+
+def create_measure_rating(score):
+    """Create a new measure rating."""
+
+    measure_rating = Measure_Rating(score=score)
+
+    db.session.add(measure_rating)
+    db.session.commit()
+
+    return measure_rating
+
 if __name__ == '__main__':
     from server import app
     connect_to_db(app)
