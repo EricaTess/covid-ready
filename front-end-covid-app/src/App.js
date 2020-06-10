5dbcb263be782e54cdb169b584cb86237e021de2
Erica Sanchez <sanchez.ericat@gmail.com>
2020-06-10 13:48:35 -0700

    added info windows to markers

--- front-end-covid-app/src/App.js
+++ front-end-covid-app/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component, createRef } from 'react'
 
-// import CurrentLocation from './components/CurrentLocation'
+
 
 const placesList = [];
 
 export default class GoogleMap extends Component {
   constructor(props) {
     super(props);
-    // this.marker = this.createMarker.bind(this)
     this.state = {
       markers: [],
       currentLocation: {
@@ -15,9 +14,6 @@ export default class GoogleMap extends Component {
         lng: -122.4194
       }
     };
-
-    // this.createPlaces = this.createPlaces.bind(this);
-    // this.createMarker = this.createMarker.bind(this)
     this.googleMapRef = React.createRef();
   }
 
@@ -29,12 +25,11 @@ export default class GoogleMap extends Component {
 
     googleMapScript.addEventListener("load", () => {
       this.googleMap = this.createGoogleMap();
-      console.log(this.googleMap);
-      // this.markers = this.createMarker();
       this.places = this.createPlaces();
     });
   }
 
+  // Create the map.
   createGoogleMap = () => {
     const map = new window.google.maps.Map(this.googleMapRef.current, {
       zoom: 14,
@@ -44,91 +39,46 @@ export default class GoogleMap extends Component {
       },
       disableDefaultUI: false,
       });
-    console.log('map created')
 
     return map;
   }
 
-
-  // getClinics = () => {
-  //   const endPoint = 'https://api.tomtom.com/search/2/search/medical%20clinic.json?lat=37.7749&lon=-122.4194&key=OQGsIrsxhl8F6lxQVSp3oJIQrBvcAcZr'
-  //     fetch(endPoint)
-  //       .then(response => response.json())             
-  //       .then(data => results.push(data.results));
-  // }
-
-
-  // createMarker() {
-  //   console.log(results);
-  //   for (let i = 0; i <= results.length; i++){
-  //     // new window.google.maps.Marker({
-  //     // position: { lat: results[i].position.lat,
-  //     //             lng: results[i].position.lon },
-  //     // map: this.googleMap
-  //     // });
-  //   }
-  // } 
-
-    // fetch(endPoint)
-    //   .then(res => res.json())
-    //   .then(
-    //     (result) => {
-    //       console.log(result);
-    //       this.setState({
-    //         currentLocation: {
-    //           // lat: geometry.location.lat,
-    //           // lng: geometry.location.lng
-    //         }
-    //       })
-    //     }
-    //   )
-
-
-
   createPlaces = () => { 
 
     const request = {
-      query: 'medical clinic'
+      query: 'medical clinic',
+      location: this.state.currentLocation
     };
     console.log('places created');
-    
+
+    const infoWindow = new window.google.maps.InfoWindow();
 
     const callback = (results, status) => {
       console.log('in callback')
       if (status === window.google.maps.places.PlacesServiceStatus.OK) {
         for (let i = 0; i < results.length; i++) {
-          console.log(results[i].geometry.location.lat());
-          // placesList.push(results[i]);
-          // createMarker(results[i]);
-          new window.google.maps.Marker({
+          console.log(results[i].name);
+    
+          const marker = new window.google.maps.Marker({
             position: { lat: results[i].geometry.location.lat(),
                          lng: results[i].geometry.location.lng() },
-            map: this.googleMap
+            map: this.googleMap,
           })
+          marker.addListener('click', () => {
+            infoWindow.setContent(results[i].name)
+            infoWindow.open(this.googleMap, marker);
+          });
         }
       }
-      // console.log(placesList);
     }
-    console.log(this.googleMap);
     // const infoWindow = new window.google.maps.InfoWindow(this.googleMap);
+
+    //Create the places service.
     const service = new window.google.maps.places.PlacesService(this.googleMap);
     service.textSearch(request, callback);
   }
 
-  // createMarker = () =>
-  //   new window.google.maps.Marker({
-  //     position: { lat: 43.642567, lng: -79.387054 },
-  //     map: this.googleMap,
-  //   })
 
-  // createMarker() {
-  //   console.log(placesList);
-  //   placesList.map(new window.google.maps.Marker({
-  //       position: { placesList.geometry.location },
-  //       map: this.googleMap
-  //     })
-  //   )
-  // }
 
   // createMarker(place) {
   //     console.log(place);
