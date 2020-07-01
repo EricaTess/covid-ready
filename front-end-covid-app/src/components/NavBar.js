5810c09d767a312eace4302a47313ceb5c871aa2
EricaTess <sanchez.ericat@gmail.com>
2020-07-01 09:49:28 -0700

    added font

--- front-end-covid-app/src/components/NavBar.js
+++ front-end-covid-app/src/components/NavBar.js
@@ -7,6 +7,8 @@ const NavBar = () => {
     
   return (
     <div className="navbar m1-auto">
+      <img src="./img/facemask-icon.png"/>
+      <h1 className="app-name">COVID Ready</h1>
       <Link to="/login" onClick={() => localStorage.clear()} className="m1-auto">Log out</Link>
     </div>
   );
