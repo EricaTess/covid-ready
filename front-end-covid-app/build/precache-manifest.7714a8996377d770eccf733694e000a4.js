4124eb942185f73679debb6c24375776446997e4
EricaTess <sanchez.ericat@gmail.com>
2020-08-12 17:07:16 -0700

    created build for deployment

---
+++ front-end-covid-app/build/precache-manifest.7714a8996377d770eccf733694e000a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd74de174c719ccaa8fc5f67e51380b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25dbc77f2391e26eeb6c",
+    "url": "/static/css/main.c363abb8.chunk.css"
+  },
+  {
+    "revision": "aa35eb11e8509cdbc695",
+    "url": "/static/js/2.84ec4590.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.84ec4590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25dbc77f2391e26eeb6c",
+    "url": "/static/js/main.8fa65548.chunk.js"
+  },
+  {
+    "revision": "6574683c74112452472c",
+    "url": "/static/js/runtime-main.2a64de85.js"
+  },
+  {
+    "revision": "a4dd2ad6d57ffd7a7b9803d42782fd9e",
+    "url": "/static/media/clay-banks-e6pK_snssSY-unsplash.a4dd2ad6.jpg"
+  }
+]);
